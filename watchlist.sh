21634fe34e2e53fb68e86470433e41e90ea95d1e
gamie <gamie.lagman@gmail.com>
2017-02-02 16:39:24 +0800

    Delete watchlist.sh

--- watchlist.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-php fetch_json_ticker.php
-clear
-uptime
-mysql -u pse pse -ppse -e "select symbol, amount, percent_change, volume from stockticker where symbol in ('2GO','ALI','BDO','CNPF','CPG','FLI','GLO','JFC','MER','MWIDE','NI','PGOLD','PXP','SECB','X','CHP') order by percent_change desc;"
